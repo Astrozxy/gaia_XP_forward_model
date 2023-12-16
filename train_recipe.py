af7ade6bafc7e998805b9cc35fa3c17080dbc1fa
Astrozxy <xzhang@mpia.de>
2023-12-16 21:32:46 +0100

    Switch orders of saving results and drawing pictures

--- train_recipe.py
+++ train_recipe.py
@@ -1192,6 +1192,7 @@ def execute_recipe(data_fname, output_dir, recipe, thin=1):
                 print(f'Loading model: {fn_model}')
                 stellar_model = FluxModel.load(fn_model+'-1')
 
+
             # Skip to next step
             #continue
 
@@ -1237,6 +1238,33 @@ def execute_recipe(data_fname, output_dir, recipe, thin=1):
 
             # Checkpoint and make plots
             save_as_h5(train_hist, full_fn(f'hist_loss/hist_{step_name}.h5'))
+
+            # Save training-set stellar parameter estimates
+            fn_param = full_fn(f'data/stellar_params_train.h5')
+            idx_select = np.arange(n_train, dtype='i8')
+            save_param_est(d_train, idx_select, fn_param)
+
+            # Learn validation set parameters
+            print('Optimizing stellar parameters in validation set ...')
+            val_kwargs = get_default_args(train_stellar_model)
+            val_kwargs.update({
+                'optimize_stellar_model': False,
+                'var_update': ['atm','E','plx','xi']
+            })
+            val_kwargs.update(recipe.get('validation',{}))
+            weights_per_star = np.ones(n_val, dtype='f4')
+            ret = train_stellar_model(
+                stellar_model,
+                d_val,
+                weights_per_star,
+                **val_kwargs
+            )
+
+            # Save validation-set stellar parameter estimates
+            fn_param = full_fn(f'data/stellar_params_val.h5')
+            idx_select = np.arange(n_val, dtype='i8')
+            save_param_est(d_val, idx_select, fn_param)
+
 
         
         train_hist = load_h5(full_fn(f'hist_loss/hist_{step_name}.h5'))
@@ -1307,32 +1335,6 @@ def execute_recipe(data_fname, output_dir, recipe, thin=1):
         # Mark this stage as completed by creating an empty file
         with open(fn_completed, 'w') as f:
             f.write('')
-
-    # Save training-set stellar parameter estimates
-    fn_param = full_fn(f'data/stellar_params_train.h5')
-    idx_select = np.arange(n_train, dtype='i8')
-    save_param_est(d_train, idx_select, fn_param)
-
-    # Learn validation set parameters
-    print('Optimizing stellar parameters in validation set ...')
-    val_kwargs = get_default_args(train_stellar_model)
-    val_kwargs.update({
-        'optimize_stellar_model': False,
-        'var_update': ['atm','E','plx','xi']
-    })
-    val_kwargs.update(recipe.get('validation',{}))
-    weights_per_star = np.ones(n_val, dtype='f4')
-    ret = train_stellar_model(
-        stellar_model,
-        d_val,
-        weights_per_star,
-        **val_kwargs
-    )
-
-    # Save validation-set stellar parameter estimates
-    fn_param = full_fn(f'data/stellar_params_val.h5')
-    idx_select = np.arange(n_val, dtype='i8')
-    save_param_est(d_val, idx_select, fn_param)
 
 
 def save_param_est(d, idx_sel, fname):
